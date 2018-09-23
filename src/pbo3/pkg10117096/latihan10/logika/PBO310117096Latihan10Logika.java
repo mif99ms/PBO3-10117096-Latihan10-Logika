206afe1cce9d75ae1f0a617117ebcbf0729c0581
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-23 18:44:42 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan10/logika/PBO310117096Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA         : MIFTAH MUHAMMAD SIDQI
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program untuk menampilkan logika
+ */
+public class PBO310117096Latihan10Logika {
+
+    
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
